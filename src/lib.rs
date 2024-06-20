f4da4eccea1bfb43624d01c758f9bad3944090ce
Conrad Ludgate <conradludgate@gmail.com>
2024-06-20 19:32:32 +0100

    cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -6,63 +6,27 @@ use base64ct::{Base64UrlUnpadded, Encoding};
 use p256::ecdsa::signature::Verifier;
 use p256::ecdsa::{Signature, VerifyingKey};
 use p256::elliptic_curve::generic_array::GenericArray;
-use p256::elliptic_curve::sec1::FromEncodedPoint;
-use p256::{AffinePoint, EncodedPoint, FieldBytes};
+use p256::PublicKey;
 use pg_sys::pid_t;
 use pgrx::{prelude::*, JsonB};
-use serde::Deserialize;
 
 pgrx::pg_module_magic!();
 
 thread_local! {
     static JWK: RefCell<Option<(pid_t, VerifyingKey)>> = const { RefCell::new(None) };
-    static JWT: RefCell<JsonB> = const { RefCell::new(JsonB(serde_json::Value::Null)) };
-    static TXID: RefCell<u64> = const { RefCell::new(0) };
+    static JWT: RefCell<serde_json::Value> = const { RefCell::new(serde_json::Value::Null) };
+    static TXID: RefCell<i64> = const { RefCell::new(0) };
 }
 
 #[pg_extern]
 fn init_jwk(pid: pid_t, s: &str) {
-    /// JSON Web Key (JWK) with a `kty` of `"EC"` (elliptic curve).
-    ///
-    /// Specified in [RFC 7518 Section 6: Cryptographic Algorithms for Keys][1].
-    ///
-    /// [1]: https://tools.ietf.org/html/rfc7518#section-6
-    #[derive(Deserialize)]
-    pub struct JwkEcKey<'a> {
-        kty: &'a str,
-
-        /// The `crv` parameter which identifies a particular elliptic curve
-        /// as defined in RFC 7518 Section 6.2.1.1:
-        /// <https://tools.ietf.org/html/rfc7518#section-6.2.1.1>
-        crv: &'a str,
-
-        /// The x-coordinate of the elliptic curve point which is the public key
-        /// value associated with this JWK as defined in RFC 7518 6.2.1.2:
-        /// <https://tools.ietf.org/html/rfc7518#section-6.2.1.2>
-        x: &'a str,
-
-        /// The y-coordinate of the elliptic curve point which is the public key
-        /// value associated with this JWK as defined in RFC 7518 6.2.1.3:
-        /// <https://tools.ietf.org/html/rfc7518#section-6.2.1.3>
-        y: &'a str,
-    }
-
-    let key: JwkEcKey = serde_json::from_str(s).unwrap();
-    assert_eq!(key.kty, "EC");
-    assert_eq!(key.crv, "P-256");
-
-    let point = EncodedPoint::from_affine_coordinates(
-        &decode_base64url_fe(key.x),
-        &decode_base64url_fe(key.y),
-        false,
-    );
-
-    let point = AffinePoint::from_encoded_point(&point).unwrap();
-    let key = VerifyingKey::from_affine(point).unwrap();
     JWK.with_borrow_mut(|k| {
         if k.is_some() {
-            panic!("jwk already set")
+            panic!("JWK state can only be set once per session.")
         }
+
+        let key = PublicKey::from_jwk_str(s).unwrap();
+        let key = VerifyingKey::from(key);
         *k = Some((pid, key));
     });
 }
@@ -70,30 +34,51 @@ fn init_jwk(pid: pid_t, s: &str) {
 #[pg_extern]
 fn decrypt_jwt(s: &str) {
     let (pid, key) = JWK.with_borrow(|b| b.unwrap());
-    let (header_body, sig) = s.rsplit_once('.').unwrap();
+    let (header_payload, sig) = s.rsplit_once('.').unwrap();
 
     let mut sig_bytes = GenericArray::default();
     Base64UrlUnpadded::decode(sig, &mut sig_bytes).unwrap();
     let sig = Signature::from_bytes(&sig_bytes).unwrap();
 
-    key.verify(header_body.as_bytes(), &sig).unwrap();
+    key.verify(header_payload.as_bytes(), &sig).unwrap();
 
-    let (header, body) = header_body.split_once('.').unwrap();
+    let (header, payload) = header_payload.split_once('.').unwrap();
     let header: serde_json::Value = json_base64_decode(header);
-    let body: serde_json::Value = json_base64_decode(body);
+    let payload: serde_json::Value = json_base64_decode(payload);
 
-    assert_eq!(pid, header["pid"]);
+    let header = header.as_object().expect("JWT header must be an object");
+    let pid2 = header["pid"]
+        .as_number()
+        .expect("JWT header must contain a valid PID")
+        .as_i64()
+        .expect("JWT header must contain a valid PID");
+    if pid as i64 != pid2 {
+        panic!("PID mismatch");
+    }
 
-    JWT.set(JsonB(body));
+    let txid = header["txid"]
+        .as_number()
+        .expect("JWT header must contain a valid TXID")
+        .as_i64()
+        .expect("JWT header must contain a valid TXID");
+    TXID.with_borrow_mut(|t| {
+        if txid <= *t {
+            panic!("TXID not monotonic");
+        }
+        *t = txid;
+    });
+
+    assert!(payload.is_object(), "JWT payload must be an object");
+    JWT.set(payload);
 }
 
 #[pg_extern]
 fn neon_get(s: &str) -> JsonB {
     JWT.with_borrow(|j| {
-        if j.0.is_null() {
+        if j.is_null() {
             JsonB(serde_json::Value::Null)
         } else {
-            JsonB(j.0[s].clone())
+            JsonB(j[s].clone())
         }
     })
 }
@@ -104,29 +89,37 @@ fn json_base64_decode(s: &str) -> serde_json::Value {
     serde_json::from_reader(r).unwrap()
 }
 
-/// Decode a Base64url-encoded field element
-fn decode_base64url_fe(s: &str) -> FieldBytes {
-    let mut result = FieldBytes::default();
-    Base64UrlUnpadded::decode(s, &mut result).unwrap();
-    result
-}
-
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
-    use base64ct::{Base64UrlUnpadded, Encoding};
+    use base64::engine::general_purpose;
+    use base64::Engine;
     use p256::ecdsa::signature::Signer;
-    use p256::NistP256;
     use p256::{
         ecdsa::{Signature, SigningKey},
         elliptic_curve::JwkEcKey,
     };
+    use p256::{NistP256, PublicKey};
     use pgrx::prelude::*;
     use rand::rngs::OsRng;
     use serde_json::json;
 
+    fn encode_str(s: impl AsRef<[u8]>) -> String {
+        general_purpose::URL_SAFE_NO_PAD.encode(s)
+    }
+
+    fn sign_jwt(sk: &SigningKey, header: &str, payload: &str) -> String {
+        let header = encode_str(header);
+        let payload = encode_str(payload);
+
+        let message = format!("{header}.{payload}");
+        let sig: Signature = sk.sign(message.as_bytes());
+        let base64_sig = encode_str(sig.to_bytes());
+        format!("{message}.{base64_sig}")
+    }
+
     #[pg_test]
-    #[should_panic]
+    #[should_panic = "JWK state can only be set once per session."]
     fn init_jwk_twice() {
         let sk = SigningKey::random(&mut OsRng);
         let point = sk.verifying_key().to_encoded_point(false);
@@ -138,19 +131,45 @@ mod tests {
     }
 
     #[pg_test]
-    fn test_neon_jwt() {
+    #[should_panic = "PID mismatch"]
+    fn wrong_pid() {
         let sk = SigningKey::random(&mut OsRng);
-        let point = sk.verifying_key().to_encoded_point(false);
-        let jwk = JwkEcKey::from_encoded_point::<NistP256>(&point).unwrap();
-        let jwk = serde_json::to_string(&jwk).unwrap();
-
-        let message = "eyJwaWQiOjEsInRpZCI6MX0.eyJzdWIiOiJjb25yYWRsdWRnYXRlIiwiYXVkIjoibmVvbiJ9";
-        let sig: Signature = sk.sign(message.as_bytes());
-        let base64_sig = Base64UrlUnpadded::encode_string(&sig.to_bytes());
+        let jwk = PublicKey::from(sk.verifying_key()).to_jwk_string();
 
         crate::init_jwk(1, &jwk);
-        crate::decrypt_jwt(&format!("{message}.{base64_sig}"));
+        crate::decrypt_jwt(&sign_jwt(&sk, r#"{"pid":2,"txid":1}"#, r#"{}"#));
+    }
+
+    #[pg_test]
+    #[should_panic = "TXID not monotonic"]
+    fn wrong_txid() {
+        let sk = SigningKey::random(&mut OsRng);
+        let jwk = PublicKey::from(sk.verifying_key()).to_jwk_string();
+
+        crate::init_jwk(1, &jwk);
+        crate::decrypt_jwt(&sign_jwt(&sk, r#"{"pid":1,"txid":2}"#, r#"{}"#));
+        crate::decrypt_jwt(&sign_jwt(&sk, r#"{"pid":1,"txid":1}"#, r#"{}"#));
+    }
+
+    #[pg_test]
+    fn test_neon_jwt() {
+        let sk = SigningKey::random(&mut OsRng);
+        let jwk = PublicKey::from(sk.verifying_key()).to_jwk_string();
+
+        crate::init_jwk(1, &jwk);
+        crate::decrypt_jwt(&sign_jwt(
+            &sk,
+            r#"{"pid":1,"txid":1}"#,
+            r#"{"sub":"conradludgate"}"#,
+        ));
         assert_eq!(crate::neon_get("sub").0, json!("conradludgate"));
+
+        crate::decrypt_jwt(&sign_jwt(
+            &sk,
+            r#"{"pid":1,"txid":2}"#,
+            r#"{"sub":"not_conradludgate"}"#,
+        ));
+        assert_eq!(crate::neon_get("sub").0, json!("not_conradludgate"));
     }
 }
 
