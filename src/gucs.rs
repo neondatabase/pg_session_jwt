f5c4cf5603be81b4c696d20ceb73305d656c4555
Jakub Kołodziejczak <jakub@neon.tech>
2024-10-09 08:47:42 +0200

    init() expects JWK in runtime params

--- src/gucs.rs
+++ src/gucs.rs
@@ -1,21 +1,23 @@
 use pgrx::*;
 use std::ffi::CStr;
 
+pub static NEON_AUTH_JWK_RUNTIME_PARAM: &'static str = "neon.auth.jwk";
 pub static NEON_AUTH_JWK: GucSetting<Option<&'static CStr>> = GucSetting::<Option<&'static CStr>>::new(None);
+pub static NEON_AUTH_JWT_RUNTIME_PARAM: &'static str = "neon.auth.jwt";
 pub static NEON_AUTH_JWT: GucSetting<Option<&'static CStr>> = GucSetting::<Option<&'static CStr>>::new(None);
 
 pub fn init() {
     GucRegistry::define_string_guc(
-        "neon.auth.jwk",
+        &NEON_AUTH_JWK_RUNTIME_PARAM,
         "JSON Web Key (JWK) userd for JWT validation",
         "Generated per connection by Neon local proxy",
         &NEON_AUTH_JWK,
-        GucContext::SuBackend, GucFlags::NOT_WHILE_SEC_REST);
+        GucContext::Suset, GucFlags::NOT_WHILE_SEC_REST);
 
     GucRegistry::define_string_guc(
-        "neon.auth.jwt",
+        &NEON_AUTH_JWT_RUNTIME_PARAM,
         "JSON Web Token (JWT) used for query authorization",
-        "Represents user session related claims like user id",
+        "Represents authenticated user session related claims like user ID",
         &NEON_AUTH_JWT,
         GucContext::Suset, GucFlags::NOT_WHILE_SEC_REST);
 }
