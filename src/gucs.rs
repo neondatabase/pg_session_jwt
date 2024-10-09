d073a88c714cd3ca068b6101bba2c4f356778594
Jakub Kołodziejczak <jakub@neon.tech>
2024-10-09 09:54:16 +0200

    refactor

--- src/gucs.rs
+++ src/gucs.rs
@@ -1,14 +1,14 @@
 use pgrx::*;
 use std::ffi::CStr;
 
-pub static NEON_AUTH_JWK_RUNTIME_PARAM: &'static str = "neon.auth.jwk";
+pub static NEON_AUTH_JWK_RUNTIME_PARAM: &str = "neon.auth.jwk";
 pub static NEON_AUTH_JWK: GucSetting<Option<&'static CStr>> = GucSetting::<Option<&'static CStr>>::new(None);
-pub static NEON_AUTH_JWT_RUNTIME_PARAM: &'static str = "neon.auth.jwt";
+pub static NEON_AUTH_JWT_RUNTIME_PARAM: &str = "neon.auth.jwt";
 pub static NEON_AUTH_JWT: GucSetting<Option<&'static CStr>> = GucSetting::<Option<&'static CStr>>::new(None);
 
 pub fn init() {
     GucRegistry::define_string_guc(
-        &NEON_AUTH_JWK_RUNTIME_PARAM,
+        NEON_AUTH_JWK_RUNTIME_PARAM,
         "JSON Web Key (JWK) userd for JWT validation",
         "Generated per connection by Neon local proxy",
         &NEON_AUTH_JWK,
@@ -17,7 +17,7 @@ pub fn init() {
         );
 
     GucRegistry::define_string_guc(
-        &NEON_AUTH_JWT_RUNTIME_PARAM,
+        NEON_AUTH_JWT_RUNTIME_PARAM,
         "JSON Web Token (JWT) used for query authorization",
         "Represents authenticated user session related claims like user ID",
         &NEON_AUTH_JWT,
