b70cceadd2b16ffad025013fb57cf6d9d5053274
Jakub Kołodziejczak <31549762+mrl5@users.noreply.github.com>
2024-10-11 11:58:16 +0200

    feat: allow bgworkers to access JWT claims (#11)
    related to https://github.com/neondatabase/cloud/issues/16041
    and this checkpoint from
    https://github.com/neondatabase/pg_session_jwt/issues/6
    > Are the thread local variables supposed to be per-backend? Doesn't
    seem like they need to be thread local to me. I think this has been
    previously discussed.
    
    closes #6
    
    ---------
    
    Co-authored-by: Conrad Ludgate <conradludgate@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod gucs;
+
 use pgrx::prelude::*;
 
 pgrx::pg_module_magic!();
@@ -11,6 +13,12 @@ macro_rules! error_code {
         // but we want `!`.
         unreachable!()
     }};
+}
+
+#[allow(non_snake_case)]
+#[pg_guard]
+pub unsafe extern "C" fn _PG_init() {
+    gucs::init();
 }
 
 #[pg_schema]
@@ -27,18 +35,16 @@ pub mod auth {
     use pgrx::JsonB;
     use serde::de::DeserializeOwned;
 
+    use crate::gucs::{
+        NEON_AUTH_JWK, NEON_AUTH_JWK_RUNTIME_PARAM, NEON_AUTH_JWT, NEON_AUTH_JWT_RUNTIME_PARAM,
+    };
+
     type Object = serde_json::Map<String, serde_json::Value>;
 
     thread_local! {
-        static JWK: OnceCell<Key> = const { OnceCell::new() };
+        static JWK: OnceCell<VerifyingKey> = const { OnceCell::new() };
         static JWT: RefCell<Option<Object>> = const { RefCell::new(None) };
         static JTI: RefCell<i64> = const { RefCell::new(0) };
-    }
-
-    #[derive(Clone)]
-    struct Key {
-        kid: i64,
-        key: VerifyingKey,
     }
 
     /// Set the public key and key ID for this postgres session.
@@ -48,15 +54,32 @@ pub mod auth {
     /// This function will panic if called multiple times per session.
     /// This is to prevent replacing the key mid-session.
     #[pg_extern]
-    pub fn init(kid: i64, s: JsonB) {
-        let key: JwkEcKey = serde_json::from_value(s.0).unwrap_or_else(|e| {
+    pub fn init() {
+        let jwk = NEON_AUTH_JWK
+            .get()
+            .unwrap_or_else(|| {
+                error_code!(
+                    PgSqlErrorCode::ERRCODE_NO_DATA,
+                    format!("Missing runtime parameter: {}", NEON_AUTH_JWK_RUNTIME_PARAM)
+                )
+            })
+            .to_str()
+            .unwrap_or_else(|e| {
+                error_code!(
+                    PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
+                    format!("Couldn't parse {}", NEON_AUTH_JWK_RUNTIME_PARAM),
+                    e.to_string(),
+                )
+            });
+
+        let jwk: JwkEcKey = serde_json::from_str(jwk).unwrap_or_else(|e| {
             error_code!(
                 PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
                 "session init requires an ES256 JWK",
                 e.to_string(),
             )
         });
-        let key = PublicKey::from_jwk(&key).unwrap_or_else(|p256::elliptic_curve::Error| {
+        let key = PublicKey::from_jwk(&jwk).unwrap_or_else(|p256::elliptic_curve::Error| {
             error_code!(
                 PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
                 "session init requires an ES256 JWK",
@@ -64,7 +87,7 @@ pub mod auth {
         });
         let key = VerifyingKey::from(key);
         JWK.with(|j| {
-            if j.set(Key { kid, key }).is_err() {
+            if j.set(key).is_err() {
                 error_code!(
                     PgSqlErrorCode::ERRCODE_UNIQUE_VIOLATION,
                     "JWK state can only be set once per session.",
@@ -73,7 +96,7 @@ pub mod auth {
         })
     }
 
-    fn verify_signature(key: &Key, body: &str, sig: &str) {
+    fn verify_signature(key: &VerifyingKey, body: &str, sig: &str) {
         let mut sig_bytes = GenericArray::default();
         Base64UrlUnpadded::decode(sig, &mut sig_bytes).unwrap_or_else(|_| {
             error_code!(
@@ -88,28 +111,12 @@ pub mod auth {
             )
         });
 
-        key.key.verify(body.as_bytes(), &sig).unwrap_or_else(|_| {
+        key.verify(body.as_bytes(), &sig).unwrap_or_else(|_| {
             error_code!(
                 PgSqlErrorCode::ERRCODE_CHECK_VIOLATION,
                 "invalid JWT signature",
             )
         });
-    }
-
-    fn verify_key_id(key: &Key, header: &Object) {
-        let kid = header
-            .get("kid")
-            .and_then(|x| x.as_i64())
-            .unwrap_or_else(|| {
-                error_code!(
-                    PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
-                    "JWT header must contain a valid 'kid' (key ID)",
-                )
-            });
-
-        if key.kid != kid {
-            error_code!(PgSqlErrorCode::ERRCODE_CHECK_VIOLATION, "Key ID mismatch");
-        }
     }
 
     fn verify_token_id(payload: &Object) -> i64 {
@@ -185,32 +192,63 @@ pub mod auth {
     ///
     /// This function will panic if the JWT could not be verified.
     #[pg_extern]
-    pub fn jwt_session_init(s: &str) {
-        let key = JWK.with(|b| {
-            b.get()
-                .unwrap_or_else(|| {
-                    error_code!(
-                        PgSqlErrorCode::ERRCODE_NOT_NULL_VIOLATION,
-                        "JWK state has not been initialised",
-                    )
-                })
-                .clone()
-        });
-        let (body, sig) = s.rsplit_once('.').unwrap_or_else(|| {
+    pub fn jwt_session_init(jwt: &str) {
+        Spi::run(
+            format!(
+                "SET {} = {}",
+                NEON_AUTH_JWT_RUNTIME_PARAM,
+                spi::quote_literal(jwt)
+            )
+            .as_str(),
+        )
+        .unwrap_or_else(|e| {
             error_code!(
-                PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
-                "invalid JWT encoding",
+                PgSqlErrorCode::ERRCODE_S_R_E_PROHIBITED_SQL_STATEMENT_ATTEMPTED,
+                format!("Couldn't set {}", NEON_AUTH_JWT_RUNTIME_PARAM),
+                e.to_string(),
             )
         });
-        let (header, payload) = body.split_once('.').unwrap_or_else(|| {
-            error_code!(
-                PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
-                "invalid JWT encoding",
-            )
-        });
-        let header: Object = json_base64_decode(header);
+        set_jwt_cache()
+    }
 
-        verify_key_id(&key, &header);
+    fn set_jwt_cache() {
+        let jwt = NEON_AUTH_JWT
+            .get()
+            .unwrap_or_else(|| {
+                error_code!(
+                    PgSqlErrorCode::ERRCODE_NO_DATA,
+                    format!("Missing runtime parameter: {}", NEON_AUTH_JWT_RUNTIME_PARAM)
+                )
+            })
+            .to_str()
+            .unwrap_or_else(|e| {
+                error_code!(
+                    PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
+                    format!("Couldn't parse {}", NEON_AUTH_JWT_RUNTIME_PARAM),
+                    e.to_string(),
+                )
+            });
+        let key = JWK.with(|b| {
+            *b.get().unwrap_or_else(|| {
+                error_code!(
+                    PgSqlErrorCode::ERRCODE_NOT_NULL_VIOLATION,
+                    "JWK state has not been initialised",
+                )
+            })
+        });
+        let (body, sig) = jwt.rsplit_once('.').unwrap_or_else(|| {
+            error_code!(
+                PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
+                "invalid JWT encoding",
+            )
+        });
+        let (_, payload) = body.split_once('.').unwrap_or_else(|| {
+            error_code!(
+                PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
+                "invalid JWT encoding",
+            )
+        });
+
         verify_signature(&key, body, sig);
 
         let payload: Object = json_base64_decode(payload);
@@ -224,20 +262,28 @@ pub mod auth {
 
     /// Extract a value from the shared state.
     #[pg_extern]
-    pub fn session(s: &str) -> JsonB {
+    pub fn session() -> JsonB {
+        JWK.with(|j| {
+            if j.get().is_none() {
+                // assuming that running as bgworker
+                init();
+                set_jwt_cache();
+            }
+        });
+
         JWT.with_borrow(|j| {
             JsonB(
                 j.as_ref()
-                    .and_then(|j| j.get(s).cloned())
-                    .unwrap_or(serde_json::Value::Null),
+                    .cloned()
+                    .map_or(serde_json::Value::Null, serde_json::Value::Object),
             )
         })
     }
 
     #[pg_extern]
-    pub fn user_id() -> String {
-        match session("sub").0 {
-            serde_json::Value::String(s) => s,
+    pub fn user_id() -> Option<String> {
+        match session().0.get("sub")? {
+            serde_json::Value::String(s) => Some(s.clone()),
             _ => error_code!(
                 PgSqlErrorCode::ERRCODE_DATATYPE_MISMATCH,
                 "invalid subject claim in the JWT"
@@ -260,164 +306,5 @@ pub mod auth {
                 e.to_string(),
             )
         })
-    }
-}
-
-#[cfg(any(test, feature = "pg_test"))]
-#[pg_schema]
-mod tests {
-    use std::fmt::Display;
-    use std::time::{SystemTime, UNIX_EPOCH};
-
-    use base64ct::{Base64UrlUnpadded, Encoding};
-    use p256::ecdsa::signature::Signer;
-    use p256::{
-        ecdsa::{Signature, SigningKey},
-        elliptic_curve::JwkEcKey,
-    };
-    use p256::{NistP256, PublicKey};
-    use pgrx::{prelude::*, JsonB};
-    use rand::rngs::OsRng;
-    use serde_json::json;
-
-    use crate::auth;
-
-    fn sign_jwt(sk: &SigningKey, header: &str, payload: impl Display) -> String {
-        let header = Base64UrlUnpadded::encode_string(header.as_bytes());
-        let payload = Base64UrlUnpadded::encode_string(payload.to_string().as_bytes());
-
-        let message = format!("{header}.{payload}");
-        let sig: Signature = sk.sign(message.as_bytes());
-        let base64_sig = Base64UrlUnpadded::encode_string(&sig.to_bytes());
-        format!("{message}.{base64_sig}")
-    }
-
-    #[pg_test]
-    #[should_panic = "JWK state can only be set once per session."]
-    fn init_jwk_twice() {
-        let sk = SigningKey::random(&mut OsRng);
-        let point = sk.verifying_key().to_encoded_point(false);
-        let jwk = JwkEcKey::from_encoded_point::<NistP256>(&point).unwrap();
-        let jwk = serde_json::to_value(&jwk).unwrap();
-
-        auth::init(1, JsonB(jwk.clone()));
-        auth::init(2, JsonB(jwk));
-    }
-
-    #[pg_test]
-    #[should_panic = "Key ID mismatch"]
-    fn wrong_pid() {
-        let sk = SigningKey::random(&mut OsRng);
-        let jwk = PublicKey::from(sk.verifying_key()).to_jwk();
-        let jwk = JsonB(serde_json::to_value(&jwk).unwrap());
-
-        auth::init(1, jwk);
-        auth::jwt_session_init(&sign_jwt(&sk, r#"{"kid":2}"#, r#"{"jti":1}"#));
-    }
-
-    #[pg_test]
-    #[should_panic = "Token ID must be strictly monotonically increasing"]
-    fn wrong_txid() {
-        let sk = SigningKey::random(&mut OsRng);
-        let jwk = PublicKey::from(sk.verifying_key()).to_jwk();
-        let jwk = JsonB(serde_json::to_value(&jwk).unwrap());
-
-        auth::init(1, jwk);
-        auth::jwt_session_init(&sign_jwt(&sk, r#"{"kid":1}"#, r#"{"jti":2}"#));
-        auth::jwt_session_init(&sign_jwt(&sk, r#"{"kid":1}"#, r#"{"jti":1}"#));
-    }
-
-    #[pg_test]
-    #[should_panic = "Token used before it is ready"]
-    fn invalid_nbf() {
-        let sk = SigningKey::random(&mut OsRng);
-        let jwk = PublicKey::from(sk.verifying_key()).to_jwk();
-        let jwk = JsonB(serde_json::to_value(&jwk).unwrap());
-
-        auth::init(1, jwk);
-
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        auth::jwt_session_init(&sign_jwt(
-            &sk,
-            r#"{"kid":1}"#,
-            json!({"jti": 1, "nbf": now + 10}),
-        ));
-    }
-
-    #[pg_test]
-    #[should_panic = "Token used after it has expired"]
-    fn invalid_exp() {
-        let sk = SigningKey::random(&mut OsRng);
-        let jwk = PublicKey::from(sk.verifying_key()).to_jwk();
-        let jwk = JsonB(serde_json::to_value(&jwk).unwrap());
-
-        auth::init(1, jwk);
-
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-        auth::jwt_session_init(&sign_jwt(
-            &sk,
-            r#"{"kid":1}"#,
-            json!({"jti": 1, "nbf": now - 10, "exp": now - 5}),
-        ));
-    }
-
-    #[pg_test]
-    fn valid_time() {
-        let sk = SigningKey::random(&mut OsRng);
-        let jwk = PublicKey::from(sk.verifying_key()).to_jwk();
-        let jwk = JsonB(serde_json::to_value(&jwk).unwrap());
-
-        auth::init(1, jwk);
-
-        let now = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs();
-
-        let header = r#"{"kid":1}"#;
-
-        auth::jwt_session_init(&sign_jwt(
-            &sk,
-            header,
-            json!({"jti": 1, "nbf": now - 10, "exp": now + 10}),
-        ));
-        auth::jwt_session_init(&sign_jwt(&sk, header, json!({"jti": 2, "nbf": now - 10})));
-        auth::jwt_session_init(&sign_jwt(&sk, header, json!({"jti": 3, "exp": now + 10})));
-    }
-
-    #[pg_test]
-    fn test_pg_session_jwt() {
-        let sk = SigningKey::random(&mut OsRng);
-        let jwk = PublicKey::from(sk.verifying_key()).to_jwk();
-        let jwk = JsonB(serde_json::to_value(&jwk).unwrap());
-
-        auth::init(1, jwk);
-        let header = r#"{"kid":1}"#;
-
-        auth::jwt_session_init(&sign_jwt(&sk, header, r#"{"sub":"foo","jti":1}"#));
-        assert_eq!(auth::user_id(), "foo");
-
-        auth::jwt_session_init(&sign_jwt(&sk, header, r#"{"sub":"bar","jti":2}"#));
-        assert_eq!(auth::user_id(), "bar");
-    }
-}
-
-/// This module is required by `cargo pgrx test` invocations.
-/// It must be visible at the root of your extension crate.
-#[cfg(test)]
-pub mod pg_test {
-    pub fn setup(_options: Vec<&str>) {
-        // perform one-off initialization when the pg_test framework starts
-    }
-
-    pub fn postgresql_conf_options() -> Vec<&'static str> {
-        // return any postgresql.conf settings that are required for your tests
-        vec![]
     }
 }
---
+++ tests/pg_session_jwt.rs
@@ -0,0 +1,202 @@
+use std::process::ExitCode;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use base64ct::{Base64UrlUnpadded, Encoding};
+use libtest_mimic::{run, Trial};
+use p256::ecdsa::signature::Signer;
+use p256::{
+    ecdsa::{Signature, SigningKey},
+    elliptic_curve::JwkEcKey,
+    NistP256,
+};
+use rand::rngs::OsRng;
+use serde_json::json;
+
+fn main() -> ExitCode {
+    let mut args = libtest_mimic::Arguments::from_args();
+    // fixes concurrent update failures
+    args.test_threads = Some(1);
+
+    let mut tests = vec![];
+
+    let err = "Token ID must be strictly monotonically increasing.";
+    tests.push(test_fn("wrong_txid", Some(err), wrong_txid));
+
+    let err = "Token used before it is ready";
+    tests.push(test_fn("invalid_nbf", Some(err), invalid_nbf));
+
+    let err = "Token used after it has expired";
+    tests.push(test_fn("invalid_exp", Some(err), invalid_exp));
+
+    tests.push(test_fn("valid_time", None, valid_time));
+    tests.push(test_fn("test_pg_session_jwt", None, test_pg_session_jwt));
+    tests.push(test_fn("test_bgworker", None, test_bgworker));
+
+    run(&args, tests).exit_code()
+}
+
+// bgworker process exits after execution, because of that we don't need to test case for more
+// than one JWT
+fn test_fn<F>(name: &str, error: Option<&'static str>, f: F) -> Trial
+where
+    F: for<'a, 'b> FnOnce(&'a SigningKey, &'b mut postgres::Client) -> Result<(), postgres::Error>
+        + Send
+        + 'static,
+{
+    let sk = SigningKey::random(&mut OsRng);
+    let jwk = create_jwk(&sk);
+    let options = format!("-c {NEON_AUTH_JWK_RUNTIME_PARAM}={jwk}");
+
+    Trial::test(name, move || {
+        pgrx_tests::run_test(Some(&options), error, vec![], move |tx| f(&sk, tx))
+            .map_err(libtest_mimic::Failed::from)
+    })
+}
+
+fn wrong_txid(sk: &SigningKey, tx: &mut postgres::Client) -> Result<(), postgres::Error> {
+    let jwt1 = sign_jwt(sk, r#"{"kid":1}"#, r#"{"jti":1}"#);
+    let jwt2 = sign_jwt(sk, r#"{"kid":1}"#, r#"{"jti":2}"#);
+
+    tx.execute("select auth.init()", &[])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt2])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt1])?;
+
+    Ok(())
+}
+
+fn invalid_nbf(sk: &SigningKey, tx: &mut postgres::Client) -> Result<(), postgres::Error> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let jwt = sign_jwt(sk, r#"{"kid":1}"#, json!({"jti": 1, "nbf": now + 10}));
+
+    tx.execute("select auth.init()", &[])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt])?;
+
+    Ok(())
+}
+
+fn invalid_exp(sk: &SigningKey, tx: &mut postgres::Client) -> Result<(), postgres::Error> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let jwt = sign_jwt(
+        sk,
+        r#"{"kid":1}"#,
+        json!({"jti": 1,  "nbf": now - 10, "exp": now - 5}),
+    );
+
+    tx.execute("select auth.init()", &[])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt])?;
+
+    Ok(())
+}
+
+fn valid_time(sk: &SigningKey, tx: &mut postgres::Client) -> Result<(), postgres::Error> {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let header = r#"{"kid":1}"#;
+    let jwt1 = sign_jwt(
+        sk,
+        header,
+        json!({"jti": 1, "nbf": now - 10, "exp": now + 10}),
+    );
+    let jwt2 = sign_jwt(sk, header, json!({"jti": 2, "nbf": now - 10}));
+    let jwt3 = sign_jwt(sk, header, json!({"jti": 3, "exp": now + 10}));
+
+    tx.execute("select auth.init()", &[])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt1])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt2])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt3])?;
+
+    Ok(())
+}
+
+fn test_pg_session_jwt(sk: &SigningKey, tx: &mut postgres::Client) -> Result<(), postgres::Error> {
+    let header = r#"{"kid":1}"#;
+    let jwt1 = sign_jwt(sk, header, r#"{"sub":"foo","jti":1}"#);
+    let jwt2 = sign_jwt(sk, header, r#"{"sub":"bar","jti":2}"#);
+
+    tx.execute("select auth.init()", &[])?;
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt1])?;
+    let user_id = tx.query_one("select auth.user_id()", &[])?;
+    let user_id = user_id.get::<_, String>("user_id");
+    assert_eq!(user_id, "foo");
+
+    tx.execute("select auth.jwt_session_init($1)", &[&jwt2])?;
+    let user_id = tx.query_one("select auth.user_id()", &[])?;
+    let user_id = user_id.get::<_, String>("user_id");
+    assert_eq!(user_id, "bar");
+
+    Ok(())
+}
+
+// bgworker process exits after execution, because of that we don't need to test case for more
+// than one JWT
+fn test_bgworker(sk: &SigningKey, tx: &mut postgres::Client) -> Result<(), postgres::Error> {
+    let header = r#"{"kid":1}"#;
+    let jwt = sign_jwt(sk, header, r#"{"sub":"foo","jti":1}"#);
+
+    tx.execute(&format!("set neon.auth.jwt = '{jwt}'"), &[])?;
+    let user_id = tx.query_one("select auth.user_id()", &[])?;
+    let user_id = user_id.get::<_, String>("user_id");
+    assert_eq!(user_id, "foo");
+
+    Ok(())
+}
+
+// fn discard() -> eyre::Result<()> {
+//     let sk = SigningKey::random(&mut OsRng);
+//     let jwk = create_jwk(&sk, 1);
+//     let options = format!("-c {NEON_AUTH_JWK_RUNTIME_PARAM}={jwk}");
+
+//     let header = r#"{"kid":1}"#;
+//     let jwt1 = sign_jwt(&sk, header, r#"{"sub":"foo","jti":1}"#);
+//     let jwt2 = sign_jwt(&sk, header, r#"{"sub":"bar","jti":2}"#);
+
+//     pgrx_tests::run_test(Some(&options), None, vec![], |tx| {
+//         tx.execute("select auth.init()", &[])?;
+//         tx.execute("select auth.jwt_session_init($1)", &[&jwt1])?;
+//         let user_id = tx.query_one("select auth.user_id()", &[])?;
+//         let user_id = user_id.get::<_, Option<String>>("user_id");
+//         assert_eq!(user_id.as_deref(), Some("foo"));
+
+//         tx.simple_query("reset neon.auth.jwt")?;
+
+//         let user_id = tx.query_one("select auth.user_id()", &[])?;
+//         let user_id = user_id.get::<_, Option<String>>("user_id");
+//         assert_eq!(user_id.as_deref(), None);
+
+//         tx.execute("select auth.jwt_session_init($1)", &[&jwt2])?;
+//         let user_id = tx.query_one("select auth.user_id()", &[])?;
+//         let user_id = user_id.get::<_, Option<String>>("user_id");
+//         assert_eq!(user_id.as_deref(), Some("bar"));
+
+//         Ok(())
+//     })
+// }
+
+static NEON_AUTH_JWK_RUNTIME_PARAM: &str = "neon.auth.jwk";
+
+fn sign_jwt(sk: &SigningKey, header: &str, payload: impl ToString) -> String {
+    let header = Base64UrlUnpadded::encode_string(header.as_bytes());
+    let payload = Base64UrlUnpadded::encode_string(payload.to_string().as_bytes());
+
+    let message = format!("{header}.{payload}");
+    let sig: Signature = sk.sign(message.as_bytes());
+    let base64_sig = Base64UrlUnpadded::encode_string(&sig.to_bytes());
+    format!("{message}.{base64_sig}")
+}
+
+fn create_jwk(sk: &SigningKey) -> String {
+    let point = sk.verifying_key().to_encoded_point(false);
+    let key = JwkEcKey::from_encoded_point::<NistP256>(&point).unwrap();
+    serde_json::to_string(&key).unwrap()
+}
